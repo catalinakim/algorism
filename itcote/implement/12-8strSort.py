b2b277c5f5a1bec4e13843a53aa6181e7381e37b
sesangmak@gmail.com <sesangmak@gmail.com>
2022-12-07 04:09:03 +0900

    12-8 해설듣고 수정

--- itcote/implement/12-8strSort.py
+++ itcote/implement/12-8strSort.py
@@ -1,18 +1,26 @@
 st = input()
 stList = []
 sumN = 0
+hasDisit = False
 for s in st:
     if s.isalpha():
         stList.append(s)
     elif s.isdigit():
         sumN += int(s)
+        hasDisit = True
     else:
         print('not both')
 stList.sort()
 # stList.append(sumN)  -> join시: expected str instance, int found
 print(stList)
 
-print(''.join(stList) + str(sumN))
+# T.숫자가 없는 경우~!
+result = ''
+if hasDisit:
+    result = ''.join(stList) + str(sumN)
+else:
+    result = ''.join(stList)
+print(result)
 
 # K1KA5CB7
 
